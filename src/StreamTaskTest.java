c6b71d6650367dd77d16ba6214272fa07a593a97
Lidia Tkach <tkach.lidia61@gmail.com>
2020-05-31 02:02:39 +0300

    Stream Homework completed

--- src/StreamTaskTest.java
+++ src/StreamTaskTest.java
@@ -19,8 +19,16 @@ public class StreamTaskTest {
                 {
                         new int[]{13, -2, 33, -4, 5},
                         20
+                },
+                {
+                        new int[]{-1, 8, 13, 6, 10, 23},
+                        200
+                },
+                {
+                        new int[]{0, -3, 11, 999},
+                        0
+
                 }
-                // TODO add 2 more test data here
         };
     }
 
@@ -38,8 +46,15 @@ public class StreamTaskTest {
                 {
                         new int[]{130, -200, 330, -40, 50, 66},
                         new int[]{}
+                },
+                {
+                        new int[]{987, 654, 321, 0},
+                        new int[]{321, 987}
+                },
+                {
+                        new int[]{9, 9, 9, 4, 4, 4, 1, 1, 1},
+                        new int[]{1, 1, 1, 9, 9, 9}
                 }
-                // TODO add 2 more test data here
         };
     }
 
